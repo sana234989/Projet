0584b1b9ba5e2ba6786f402567da200ee0610833
zineb talmati <zinebtalmati@gmail.com>
2024-02-14 12:26:32 +0100

    Sauvegarde des modifications locales dans routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\homeController; 
 use App\Http\Controllers\ReservationController;
 use App\Http\Controllers\roomscontroller;
+use App\Http\Controllers\roomsdetailcontroller;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,6 +19,25 @@ use App\Http\Controllers\roomscontroller;
 
 
 
-Route::get('/', [homecontroller::class,'index']);
+Route::get('/home', [homecontroller::class,'index']);
 route::get('/restauration', [ReservationController::class,'index']);
 route::get('/rooms', [roomscontroller::class,'index']);
+route::get('/roomsdetail', [roomsdetailcontroller::class,'index']);
+Route::get('/roomdetail1', function () {
+    return view('roomdetail1');
+});
+Route::get('/roomdetails2', function () {
+    return view('roomdetails2');
+});
+Route::get('/roomdetail3', function () {
+    return view('roomdetail3');
+});
+Route::get('/roomdetail4', function () {
+    return view('roomdetail4');
+});
+Route::get('/roomdetail5', function () {
+    return view('roomdetail5');
+});
+Route::get('/roomdetail6', function () {
+    return view('roomdetail6');
+});
