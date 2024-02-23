3694e4355697337d05d8b0dd450fefa0cd14c442
zineb talmati <zinebtalmati@gmail.com>
2024-02-23 15:13:14 +0100

    seederrestau

--- database/seeders/restaurantseeder.php
+++ database/seeders/restaurantseeder.php
@@ -14,19 +14,23 @@ class restaurantseeder extends Seeder
     public function run(): void
     {
      $restaurants=
-     [[
+     [
+      [
+         'title'=>"French restaurant",
+         'description'=> "  La cuisine française fait référence à divers styles gastronomiques dérivés de la tradition française. Elle a évolué au cours des siècles, suivant ainsi les changements sociaux et politiques du pays.",
+         'url'=>'image/frensh.png'
+      ],
+      [
+         'title'=>"Chinese restaurant",
+         'description'=> "Dans les restaurants de style chinois, les tables, habituellement de forme arrondie, sont équipées, d'un large plateau central tournant, très pratique pour atteindre chaque plat. Les sanitaires.",
+         'url'=>'image/chinoise.png'
+      ],
+
+   [
         'title'=>"Marocain restaurant",
         'description'=> "Un restaurant marocain offre une expérience culinaire riche et variée, imprégnée des saveurs exotiques et des traditions de la cuisine du Maroc. Les plats sont souvent préparés avec des ingrédients frais et des épices aromatiques.",
         'url'=>'image/marocain2.jpg'
-     ],[
-        'title'=>"French restaurant",
-        'description'=> "  La cuisine française fait référence à divers styles gastronomiques dérivés de la tradition française. Elle a évolué au cours des siècles, suivant ainsi les changements sociaux et politiques du pays.",
-        'url'=>'image/frensh.png'
      ], [
-        'title'=>"Chinese restaurant",
-        'description'=> "Dans les restaurants de style chinois, les tables, habituellement de forme arrondie, sont équipées, d'un large plateau central tournant, très pratique pour atteindre chaque plat. Les sanitaires.",
-        'url'=>'image/chinoise.png'
-     ],[
         'title'=>"Espagnol restaurant",
         'description'=>"La cuisine espagnol fait référence à divers styles gastronomiques dérivés de la tradition française. Elle a évolué au cours des siècles, suivant ainsi les changements sociaux et politiques du pays",
         'url'=>'image/espagnol.png'
