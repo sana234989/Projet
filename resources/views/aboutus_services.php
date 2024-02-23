38740532769e762dc3437ce15b5ad6de8117e800
zineb talmati <zinebtalmati@gmail.com>
2024-02-23 13:12:13 +0100

    la page about us

---
+++ resources/views/aboutus_services.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link href="https://fonts.googleapis.com/css?family=Libre+Baskerville&display=swap" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css?family=Inter&display=swap" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css?family=Libre+Caslon+Text&display=swap" rel="stylesheet" />
+ 
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Document</title>
+</head>
+
+
+<div class="text-center">
+  <h1 class="font-serif text-lg  ">The Small Detail Make the Difference</h1>
+  <p class="text-sm text-dark-500 mr-9 ml-9"> chaque détail est soigneusement pensé pour offrir une expérience immersive dans la riche culture du pays.  Retrouvez l'harmonie
+            intérieure dans notre spa d'hôtel, un sanctuaire dédié à la relaxation et au renouveau. Plongez dans une
+            expérience sensorielle .
+
+</p>
+</div>
+    <div class="flex flex-row mt-9">
+    <div class="basis-1/8 content-center"></div>
+    <div class="basis-1/3 content-center bg-slate-50  "> <div class="grid gap-4 col-start-1 col-end-3 row-start-1 sm:mb-6 sm:grid-cols-4 lg:gap-6 lg:col-start-2 lg:row-end-6 lg:row-span-6 lg:mb-0">
+            <img src="images/v19_10.png" alt="" class="w-full h-60 object-cover rounded-lg sm:h-80 sm:col-span-2 lg:col-span-full" loading="lazy">
+           
+          </div>
+          <button class=" px-4 py-1 text-sm text-dark-600 font-semibold rounded-full border border-dark-200 hover:text-white hover:bg-lime-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-lime-600 focus:ring-offset-2 ml-2 mt-2 ">Know More</button>
+        </div>
+          <div class="basis-1/12 content-center"></div>
+          <div class="basis-1/3 content-center"> <div class="grid gap-4 col-start-1 col-end-3 row-start-1 sm:mb-6 sm:grid-cols-4 lg:gap-6 lg:col-start-2 lg:row-end-6 lg:row-span-6 lg:mb-0">
+            <img src="images/v19_9.png" alt="" class="w-full h-60 object-cover rounded-lg sm:h-80 sm:col-span-2 lg:col-span-full" loading="lazy">
+            
+          </div></div>
+          <div class="basis-1/12 content-center"></div>
+          <div class="basis-1/3 content-center"> <div class="grid gap-4 col-start-1 col-end-3 row-start-1 sm:mb-6 sm:grid-cols-4 lg:gap-6 lg:col-start-2 lg:row-end-6 lg:row-span-6 lg:mb-0">
+            <img src="images/v19_8.png" alt="" class="w-full h-60 object-cover rounded-lg sm:h-80 sm:col-span-2 lg:col-span-full" loading="lazy">
+           
+          </div></div>
+          <div class="basis-1/8 content-center"></div>
+    </div>
+    <div class="flex flex-row mt-9">
+        <div class="basis-1/2 content-center"> <div class="grid gap-4 col-start-1 col-end-3 row-start-1 sm:mb-6 sm:grid-cols-4 lg:gap-6 lg:col-start-2 lg:row-end-6 lg:row-span-6 lg:mb-0">
+            <img src="images/v122_30.png" alt="" class="w-full h-60 object-cover rounded-lg sm:h-80 sm:col-span-2 lg:col-span-full" loading="lazy">
+           
+          </div></div>
+         
+         
+        <div class="basis-1/2 text-center">  <div class="mt-4 col-start-1 row-start-3 self-center sm:mt-0 sm:col-start-2 sm:row-start-2 sm:row-span-2 lg:mt-6 lg:col-start-1 lg:row-start-3 lg:row-end-4">
+            <h1  class="font-serif text-lg  text-lime-600">Spa and Beauty</h1>
+         
+        </div>
+        <p class="mt-4 text-sm leading-6 col-start-1 sm:col-span-2 lg:mt-6 lg:row-start-4 lg:col-span-1 dark:text-slate-400">
+        Retrouvez l'harmonie
+            intérieure dans notre spa d'hôtel, un sanctuaire dédié à la relaxation et au renouveau. Plongez dans une
+            expérience sensorielle .
+        </p>
+        <button type="button" class="bg-lime-200 text-dark text-sm leading-6 font-medium py-2 px-3 rounded-lg">Check availability</button>
+      </div>
+    </div>
+      
+       
+
+
+
+
+
+
+
+    <div class="flex flex-row">
+    <div class="basis-1/2 text-center">  <div class="mt-4 col-start-1 row-start-3 self-center sm:mt-0 sm:col-start-2 sm:row-start-2 sm:row-span-2 lg:mt-6 lg:col-start-1 lg:row-start-3 lg:row-end-4">
+        <h1  class="font-serif text-lg  text-lime-600">Espace evenement</h1>
+     
+    </div>
+    <p class="mt-4 text-sm leading-6 col-start-1 sm:col-span-2 lg:mt-6 lg:row-start-4 lg:col-span-1 dark:text-slate-400">
+        Créez des moments d'exception dans
+        notre espace dédié aux événements au sein de l'hôtel. Doté d'une élégance contemporaine, notre lieu offre
+        une toile parfaite pour des réunions d'affaires ...
+    </p>
+    <button type="button" class="bg-lime-200 text-dark text-sm leading-6 font-medium py-2 px-3 rounded-lg">Check availability</button>
+  </div>
+  
+  <div class="basis-1/2"> <div class="grid gap-4 col-start-1 col-end-3 row-start-1 sm:mb-6 sm:grid-cols-4 lg:gap-6 lg:col-start-2 lg:row-end-6 lg:row-span-6 lg:mb-0">
+    <img src="images/v49_92.png" alt="" class="w-full h-60 object-cover rounded-lg sm:h-80 sm:col-span-2 lg:col-span-full" loading="lazy">
+   
+  </div></div>
+
+    </div>
+
+
+
+
+    <div class="flex flex-row mt-9">
+        <div class="basis-1/2"> <div class="grid gap-4 col-start-1 col-end-3 row-start-1 sm:mb-6 sm:grid-cols-4 lg:gap-6 lg:col-start-2 lg:row-end-6 lg:row-span-6 lg:mb-0">
+            <img src="images/v49_88.png" alt="" class="w-full h-80 object-cover rounded-lg sm:h-80 sm:col-span-2 lg:col-span-full" loading="lazy">
+           
+          </div></div>
+          
+         
+        <div class="basis-1/2 text-center">  <div class="mt-4 col-start-1 row-start-3 self-center sm:mt-0 sm:col-start-2 sm:row-start-2 sm:row-span-2 lg:mt-6 lg:col-start-1 lg:row-start-3 lg:row-end-4">
+            <h1  class="font-serif text-lg  text-lime-600">Restaurant</h1>
+         
+        </div>
+        <p class="mt-4 text-sm leading-6 col-start-1 sm:col-span-2 lg:mt-6 lg:row-start-4 lg:col-span-1 dark:text-slate-400 hover:text-dark">
+        Explorez une symphonie de
+            saveurs dans notre restaurant d'hôtel, alliant cuisine exquise et élégance sans pareille. Une expérience
+            culinaire inoubliable vous attend.
+        </p>
+        <button type="button" class="bg-lime-200 text-dark text-sm leading-6 font-medium py-2 px-3 rounded-lg ">Check availability</button>
+      </div>
+    </div>
+
+   
+
+
+
+
+
+
+
+
+    <div class="flex flex-row">
+        <div class="basis-1/2 text-center">  <div class="mt-4 col-start-1 row-start-3 self-center sm:mt-0 sm:col-start-2 sm:row-start-2 sm:row-span-2 lg:mt-6 lg:col-start-1 lg:row-start-3 lg:row-end-4">
+            <h1  class="font-serif text-lg  text-lime-600">Rooms</h1>
+         
+        </div>
+        <p class="mt-4 text-sm leading-6 col-start-1 sm:col-span-2 lg:mt-6 lg:row-start-4 lg:col-span-1 dark:text-slate-400">
+        Plongez dans le luxe ultime et
+            l'élégance intemporelle de nos chambres d'hôtel. Des espaces soigneusement conçus qui allient confort
+            moderne et charme sophistiqué.
+        </p>
+        <button type="button" class="bg-lime-200 text-dark text-sm leading-6 font-medium py-2 px-3 rounded-lg">Check availability</button>
+      </div>
+      
+
+
+      <div class="basis-1/2"> <div class="grid gap-4 col-start-1 col-end-3 row-start-1 sm:mb-6 sm:grid-cols-4 lg:gap-6 lg:col-start-2 lg:row-end-6 lg:row-span-6 lg:mb-0">
+        <img src="images/v49_87.png" alt="" class="w-full h-60 object-cover rounded-lg sm:h-80 sm:col-span-2 lg:col-span-full" loading="lazy">
+       
+      </div></div>
+    
+        </div>
+</body>
+
+</html>
---
+++ resources/views/menu_chinose.blade.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <script src="https://cdn.tailwindcss.com"></script>
+  <style>
+   
+  </style>
+</head>
+<body>
+
+
+
+
+<div class=" mt-5 text-center">
+    <h1 class=" text-center text-lg font-semibold text-lg ">Welcome to your chinose kitchen</h1>
+    <p class=" mt-20 text-center  text-sm leading-6 col-start-1 sm:col-span-2 lg:mt-6 lg:row-start-4 lg:col-span-1 dark:text-slate-400">
+    L'histoire de la cuisine chinoise est marquée par la variété et par la place centrale de l'alimentation dans la culture chinoise-han.
+    </p>
+    <button type="button" class=" mt-5 hover:scale-110 hover:bg-lime-500 duration-300  bg-lime-200 text-dark text-sm leading-6 font-medium py-2 px-3 rounded-lg shadow-2xl">Check availability</button>
+</div>
+
+</div>
+
+
+<h1 class="text-center mt-3    bg-lime-100 text-dark text-sm leading-6 font-medium py-2 px-3 rounded-lg shadow-2xl">Breakfast</h1>
+<div class="  flex flex-row mt-2">
+
+    <div class="basis-1/8 content-center"></div>
+    <div class="basis-1/3 content-center bg-slate-50  "> 
+    
+        <div class="grid gap-4 col-start-1 col-end-3 row-start-1 sm:mb-6 sm:grid-cols-4 lg:gap-6 lg:col-start-2 lg:row-end-6 lg:row-span-6 lg:mb-0">
+            <img src="menu_chinose\v74_128.png" alt="" class="w-full h-60 object-cover rounded-lg sm:h-80 sm:col-span-2 lg:col-span-full" loading="lazy">
+           
+          </div>
+          <h3 class="text-center">Fried milk<br>12.9 DH</h3>
+        </div>
+          <div class="basis-1/12 content-center"></div>
+          <div class="basis-1/3 content-center"> 
+         
+            <div class="grid gap-4 col-start-1 col-end-3 row-start-1 sm:mb-6 sm:grid-cols-4 lg:gap-6 lg:col-start-2 lg:row-end-6 lg:row-span-6 lg:mb-0">
+            <img src="menu_chinose\v74_129.png" alt="" class="w-full h-60 object-cover rounded-lg sm:h-80 sm:col-span-2 lg:col-span-full" loading="lazy">
+            
+          </div>
+          <h3 class="text-center">Mapo Tofu <br>70.3 DH</h3></div>
+          <div class="basis-1/12 content-center"></div>
+          <div class="basis-1/3 content-center"> 
+        
+            <div class="grid gap-4 col-start-1 col-end-3 row-start-1 sm:mb-6 sm:grid-cols-4 lg:gap-6 lg:col-start-2 lg:row-end-6 lg:row-span-6 lg:mb-0">
+            <img src="menu_chinose\v74_130.png" alt="" class="w-full h-60 object-cover rounded-lg sm:h-80 sm:col-span-2 lg:col-span-full" loading="lazy">
+           
+          </div>
+          <h3 class="text-center">simple breakfast<br>40 DH</h3></div>
+          <div class="basis-1/8 content-center"></div>
+    </div>
+  <h1 class="text-center mt-3 bg-lime-100 text-dark text-sm leading-6 font-medium py-2 px-3 rounded-lg shadow-2xl">Lunch</h1>
+<div class="flex flex-row mt-9">
+
+    <div class="basis-1/8 content-center"></div>
+    <div class="basis-1/3 content-center bg-slate-50  "> <div class="grid gap-4 col-start-1 col-end-3 row-start-1 sm:mb-6 sm:grid-cols-4 lg:gap-6 lg:col-start-2 lg:row-end-6 lg:row-span-6 lg:mb-0">
+            <img src="menu_chinose\v74_131.png" alt="" class="w-full h-60 object-cover rounded-lg sm:h-80 sm:col-span-2 lg:col-span-full" loading="lazy">
+           
+          </div>
+          <h3 class="text-center">Wontons<br>90 DH</h3>
+        </div>
+          <div class="basis-1/12 content-center"></div>
+          <div class="basis-1/3 content-center"> <div class="grid gap-4 col-start-1 col-end-3 row-start-1 sm:mb-6 sm:grid-cols-4 lg:gap-6 lg:col-start-2 lg:row-end-6 lg:row-span-6 lg:mb-0">
+            <img src="menu_chinose\v77_146.png" alt="" class="w-full h-60 object-cover rounded-lg sm:h-80 sm:col-span-2 lg:col-span-full" loading="lazy">
+            
+          </div>
+          <h3 class="text-center">Oodles of Noodles<br>50 DH</h3></div>
+          <div class="basis-1/12 content-center"></div>
+          <div class="basis-1/3 content-center"> <div class="grid gap-4 col-start-1 col-end-3 row-start-1 sm:mb-6 sm:grid-cols-4 lg:gap-6 lg:col-start-2 lg:row-end-6 lg:row-span-6 lg:mb-0">
+            <img src="menu_chinose\v77_147.png" alt="" class="w-full h-60 object-cover rounded-lg sm:h-80 sm:col-span-2 lg:col-span-full" loading="lazy">
+           
+          </div>
+          <h3 class="text-center">Steamed buns <br>60 DH</h3></div>
+          <div class="basis-1/8 content-center"></div>
+    </div>
+    <h1 class="text-center mt-3 bg-lime-100 text-dark text-sm leading-6 font-medium py-2 px-3 rounded-lg shadow-2xl">Dinner</h1>
+<div class="flex flex-row mt-9">
+
+    <div class="basis-1/8 content-center"></div>
+    <div class="basis-1/3 content-center bg-slate-50  "> <div class="grid gap-4 col-start-1 col-end-3 row-start-1 sm:mb-6 sm:grid-cols-4 lg:gap-6 lg:col-start-2 lg:row-end-6 lg:row-span-6 lg:mb-0">
+            <img src="menu_chinose\v77_148.png" alt="" class="w-full h-60 object-cover rounded-lg sm:h-80 sm:col-span-2 lg:col-span-full" loading="lazy">
+           
+          </div>
+          <h3 class="text-center">Pancakes<br>60 DH</h3>
+        </div>
+          <div class="basis-1/12 content-center"></div>
+          <div class="basis-1/3 content-center"> <div class="grid gap-4 col-start-1 col-end-3 row-start-1 sm:mb-6 sm:grid-cols-4 lg:gap-6 lg:col-start-2 lg:row-end-6 lg:row-span-6 lg:mb-0">
+            <img src="menu_chinose\v77_149.png" alt="" class="w-full h-60 object-cover rounded-lg sm:h-80 sm:col-span-2 lg:col-span-full" loading="lazy">
+            
+          </div>
+          <h3 class="text-center">burger<br>80 DH</h3></div>
+          <div class="basis-1/12 content-center"></div>
+          <div class="basis-1/3 content-center"> <div class="grid gap-4 col-start-1 col-end-3 row-start-1 sm:mb-6 sm:grid-cols-4 lg:gap-6 lg:col-start-2 lg:row-end-6 lg:row-span-6 lg:mb-0">
+            <img src="menu_chinose\v77_155.png" alt="" class="w-full h-60 object-cover rounded-lg sm:h-80 sm:col-span-2 lg:col-span-full" loading="lazy">
+           
+          </div>
+          <h3 class="text-center">Rice Porridge<br>70 DH</h3></div>
+          <div class="basis-1/8 content-center"></div>
+    </div>
+    
+
+
+
+
+
+
+
+
+
+   
+
+
+
+
+
+</body>
+
+</html>
