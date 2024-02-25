16413d6b8f27dce9d829dfa864777191c43bc193
zineb talmati <zinebtalmati@gmail.com>
2024-02-25 12:30:41 +0100

    interface roomdetail2

--- resources/views/roomdetails2.blade.php
+++ resources/views/roomdetails2.blade.php
@@ -11,16 +11,16 @@
 <body>
     <div class="room-detail w-96 md:w-5/6 h-96 md:h-auto p-8 md:p-0" id="room">
         <div class="md:mr-8">
-            <img src="{{ asset('image/double.jpg') }}" alt="image1" id="img1" class="mb-4 md:mb-0 md:mr-4 w-72 h-72">
+            <img src="{{ asset($room2->image) }}" alt="image1" id="img1" class="mb-4 md:mb-0 md:mr-4 w-72 h-72">
         </div>
         <div class="md:flex-grow"id="info">
             <div class="room1">
                 <h3 class="text-xl font-bold mb-2 font-semibold " id="name">Room name</h3>
-                <h3 class="text-1xl font-bold mb-2 text-600 font-serif" id="name1">Double Room</h3>
+                <h3 class="text-1xl font-bold mb-2 text-600 font-serif" id="name1">{{$room2->type}}</h3>
             </div>
             <div class="room11">
                 <h3 class="text-xl font-bold mb-2" id="price1">Price</h3>
-                <h3 class="text-1xl font-bold mb-2 text-blue-600 font-serif" id="price2">375$/day</h3>
+                <h3 class="text-1xl font-bold mb-2 text-blue-600 font-serif" id="price2">{{$room2->prix}}/day</h3>
             </div>
             <hr class="trait">
             <div class="service1">
@@ -35,7 +35,7 @@
             <div class="btn">
                     <form action="{{route('verifydate')}}" method="GET">
                         @csrf <!-- Ajoutez le jeton CSRF pour la sécurité -->
-                        <input type="hidden" name="chambre" value="double Room"> <!-- Champ caché pour le nom de la chambre -->
+                        <input type="hidden" name="chambre" value="double room"> <!-- Champ caché pour le nom de la chambre -->
                         <div class="flex flex-col md:flex-row mb-4 "id="checkin">
                             <label class="check mr-2 md:mr-4">Check-in</label>
                             <input type="date" name="date_debut" class="border border-gray-300 px-2 py-1 rounded mb-2 md:mb-0">
@@ -45,13 +45,27 @@
                             <input type="date" name="date_fin" class="border border-gray-300 px-2 py-1 rounded mb-2 md:mb-0">
                         </div>
             
-                        <input type="hidden" name="erreur"> 
                  
                         <div class="reserv flex items-center">
-                            <button type="submit" class="mr-8 border-style: solid;" id="lien1">Check Now</button>
-                            <a href="#" id="lien2">Reserver</a>
+                           <button type="submit" class="bn5 mr-8 border-style: solid;" id="lien1">Check</button>
+                           
+                            <a href="/" class="bn5"id="lien2">Reserver</a>
                         </div>
+
                     </form>
+                    @if(Session::has('erreur'))
+                    <div class="warning p-4 mb-4 text-sm text-yellow-800 rounded-lg bg-yellow-50 dark:bg-gray-800 dark:text-yellow-300" role="alert">
+                        <span class="font-medium">Warning alert!</span>
+                        @if(Session::get('erreur') === "la chambre n est pas disponible")
+                            Cette chambre n'est pas disponible.
+                            <script>
+                                document.getElementById('lien2').classList.add('disabled');
+                            </script>
+                        @else
+                            Cette chambre est disponible.
+                        @endif
+                    </div>
+                @endif
                 </div>
             </div>
         </div>
